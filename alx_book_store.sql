8b4266118eaacfce041c81eec0354bb5f3d77a88
Feven237 <fevenwolde54@gmail.com>
2024-10-30 13:50:59 +0400

    Update alx_book_store.sql
    i change an order table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
--- Table for Books
 CREATE TABLE IF NOT EXISTS Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
@@ -11,13 +10,11 @@ CREATE TABLE IF NOT EXISTS Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Table for Authors
 CREATE TABLE IF NOT EXISTS Authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
--- Table for Customers
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
@@ -25,7 +22,6 @@ CREATE TABLE IF NOT EXISTS Customers (
     address TEXT
 );
 
--- Table for Orders
 CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
@@ -33,11 +29,11 @@ CREATE TABLE IF NOT EXISTS Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Table for
-
-
-
-    
-
-
-   
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
